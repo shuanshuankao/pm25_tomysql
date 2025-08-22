ccd1d682e1f111405e2520d9b823a1b86d9f3003
nina <shuanshuankao1222@example.com>
2025-08-22 21:22:19 +0800

    0822 完成書籍列表

--- main.py
+++ main.py
@@ -1,37 +1,59 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
-app=Flask(__name__)
+books = {
+    1: {
+        "name": "Python book",
+        "price": 299,
+        "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
+    },
+    2: {
+        "name": "Java book",
+        "price": 399,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
+    },
+    3: {
+        "name": "C# book",
+        "price": 499,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
+    },
+}
+
+
+app = Flask(__name__)
+
 
 @app.route("/bmi/height=<h>&weight=<w>")
-def get_bmi(h,w):
-    bmi=round(eval(w)/(eval(h)/100)**2,2)
+def get_bmi(h, w):
+    bmi = round(eval(w) / (eval(h) / 100) ** 2, 2)
     return f"<h1>身高:{h}cm 體重:{w}kg<br> BMI:{bmi}</h1>"
+
 
 @app.route("/books")
 @app.route("/books/id=<int:id>")
 def get_books(id=None):
     try:
-        books={1:"Python book",2:"Java book",3:"Flask book"}
+        if id == None:
+            return render_template("books.html", books=books)
 
-        if id==None:
-            return books
-    
         return books[id]
-        #return books[1]
-        #return books
-    except Exception as e :
+        # return books[1]
+        # return books
+    except Exception as e:
         return f"書籍編號錯誤:{e}"
+
 
 @app.route("/nowtime")
 def now_time():
-    time=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    #print(time)
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # print(time)
     return time
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello MuzQ !<h1>"
+    time = now_time()
+    return render_template("index.html", timeX=time, name="Muz")
+
 
 app.run(debug=True)
--- pm25.py
+++ pm25.py
@@ -1,9 +1,9 @@
 import pymysql
 import requests
 
-url="https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=datacreationdate%20desc&format=JSON"
+url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=datacreationdate%20desc&format=JSON"
 
-table_str="""
+table_str = """
 create table if not exists pm25(
 id int auto_increment primary key,
 site varchar(25),
@@ -15,22 +15,24 @@ unique key site_time (site,datacreationdate)
 )
 """
 
-sqlstr="insert ignore into pm25(site,county,pm25,datacreationdate,itemunit)\
+sqlstr = "insert ignore into pm25(site,county,pm25,datacreationdate,itemunit)\
       values(%s,%s,%s,%s,%s)"
 
-conn,cursor=None,None
+conn, cursor = None, None
+
+
 def open_db():
-    global conn,cursor
+    global conn, cursor
     try:
-        conn=pymysql.connect(
+        conn = pymysql.connect(
             host="localhost",
             # host="127.0.0.1",
             user="root",
             password="",
             port=3307,
-            database="demo"
+            database="demo",
         )
-        cursor=conn.cursor()
+        cursor = conn.cursor()
         print(conn)
         print("資料庫開啟成功!")
     except Exception as e:
@@ -42,27 +44,28 @@ def close_db():
         conn.close()
         print("資料庫關閉成功!")
 
+
 def get_open_data():
-    resp=requests.get(url,verify=False)
-    datas=resp.json()['records']
-    values=[list(data.values()) for data in datas if list(data.values())[2]!=""]
+    resp = requests.get(url, verify=False)
+    datas = resp.json()["records"]
+    values = [list(data.values()) for data in datas if list(data.values())[2] != ""]
     return values
 
 
 def write_to_sql():
     try:
-        values=get_open_data()
-        if len(values)==0:
+        values = get_open_data()
+        if len(values) == 0:
             print("目前無資料")
             return
-        
-        size=cursor.executemany(sqlstr,values)
+
+        size = cursor.executemany(sqlstr, values)
         conn.commit()
         print(f"寫入{size}筆資料成功!")
     except Exception as e:
         print(e)
 
+
 open_db()
 write_to_sql()
 close_db()
-
